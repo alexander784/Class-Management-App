de00a9a9b4a1d6e6e8e977b7a5a868376f23d242
alexander <alexandernyaga1999@gmail.com>
2024-01-23 19:20:45 +0300

    Implement Models and db

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,27 @@
 from flask import Flask
+from models import db, User,UserAttendance,Usersubject,Subject,Content
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
+
+
+
 @app.route('/')
 def index():
     return "Hello World!"
+
+
 
 
 
