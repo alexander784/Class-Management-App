cf8b07e8094d82d2f880f10bcfc56484d9151511
alexander <alexandernyaga1999@gmail.com>
2024-01-25 10:18:16 +0300

    Mdify models

--- Backend/models.py
+++ Backend/models.py
@@ -5,6 +5,7 @@ from sqlalchemy_serializer import SerializerMixin
 db = SQLAlchemy()
 
 
+## Define Models
 
 class User(db.Model, SerializerMixin):
 
@@ -15,48 +16,55 @@ class User(db.Model, SerializerMixin):
     username = db.Column(db.String)
     useremail = db.Column(db.String)
     password =db.Column(db.String)
-    isInstructor = db.Column(db.Boolean)
+    is_instructor = db.Column(db.Boolean, default=False)
+
+
+    ##Relationships
+    schedule = db.relationship("schedule", backref="user", lazy= True )
+    message = db.relationship("message", backref= "added_by", lazy= True)
+    subjects = db.relationship('Subject', backref='added_by', lazy=True)
+
 
 class Subject(db.Model):
 
     __tablename__  = "subjects"
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String)
+    code = db.Column(db.String)
+    year = db.Column(db.Integer)
+    compulsory = db.Column(db.Boolean)
+    addedby = db.Column(db.Integer,db.ForeignKey("users.id"))
 
-class Usersubject(db.Model):
+class Message(db.Model):
 
-    __tablename__  = "userSubjects"
+    __tablename__  = "messages"
 
-
-    id = db.Column(db.Integer,primary_key=True)
-    userId = db.Column(db.Integer, db.ForeignKey("users.id"))
-    subjectId = db.Column(db.Integer, db.ForeignKey("subjects.id"))
-
-class Content(db.Model):
-
-    __tablename__  = "contents"
-
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String)
-    resource = db.Column(db.String)
-
-class Attendance(db.Model):
-
-    __tablename__  = "attendances"
-
-    id = db.Column(db.Integer,primary_key=True)
-    date = db.Column(db.DateTime,server_default=db.func.now())
-    attended= db.Column(db.Boolean)
+    id = db.column(db.Integer, primary_key=True)
+    username = db.Column(db.String,)
+    addedby = db.Column(db.Integer,db.ForeignKey("users.id"))
 
 
-class UserAttendance(db.Model):
+class schedule(db.Model):
+
+     __tablename__  = "schedules"
+
+     id = db.column(db.Integer, primary_key=True)
+     day = db.Column(db.String, nullable=False)
+     time = db.Column(db.Integer, nullable=False)
+     Subject = db.Column(db.String, default="free")
 
 
-    __tablename__  = "userAttendances"
+     
 
-    id = db.Column(db.Integer,primary_key=True)
-    userId = db.Column(db.Integer,db.ForeignKey("users.id"))
-    attendanceId = db.Column(db.Integer, db.ForeignKey("attendances.id"))
+
+
+     
+
+
+
+
+
+
     
 
 
