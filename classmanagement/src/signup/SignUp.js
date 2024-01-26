c2311f8718bbef94f4dbedf406b71dc6b21d7180
alexander <alexandernyaga1999@gmail.com>
2024-01-27 01:40:32 +0300

    Store form input in state

---
+++ classmanagement/src/signup/SignUp.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import './signup.css';
+
+const SignupForm = () => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  
+};
