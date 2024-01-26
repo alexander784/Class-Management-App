21ee113beaa17c6dbf57dd816c008e1e1da3d03b
alexander <alexandernyaga1999@gmail.com>
2024-01-27 01:37:27 +0300

    Store the email and password in state

---
+++ classmanagement/src/login/Login.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import './login.css'
+import App from '../App';
+
+const LoginForm = () => {
+  // Manage email and password
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+};
