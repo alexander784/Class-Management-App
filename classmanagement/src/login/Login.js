dc125135e9387a612463e1596f1f329c09b889d9
alexander <alexandernyaga1999@gmail.com>
2024-01-27 01:38:09 +0300

    Handle forms submission

--- classmanagement/src/login/Login.js
+++ classmanagement/src/login/Login.js
@@ -6,4 +6,15 @@ const LoginForm = () => {
   // Manage email and password
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  //  handle form submission
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log('Login clicked');
+  };
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    console.log('Signup clicked');
+  };
 };
