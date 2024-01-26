e040fcca762dd7e4c6b37a8c6116eb666f5b8cd4
alexander <alexandernyaga1999@gmail.com>
2024-01-27 01:43:16 +0300

    Display the form

--- classmanagement/src/signup/SignUp.js
+++ classmanagement/src/signup/SignUp.js
@@ -16,4 +16,49 @@ const SignupForm = () => {
   const redirectToLogin = () => {
     console.log('Redirecting to Login page');
   };
+  return (
+    <div className="signup-container">
+      <h2 className="signup-title">SignUp</h2>
+      <p className='create'>First create Account</p>
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <input type="text" placeholder='Full Name'name="fullName"value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+          required
+        />
+
+        <input type="text" placeholder='Email'name="email"value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input type="password"
+          placeholder='Password'
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder='Confirm password'
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Sign Up</button>
+      </form>
+
+      <p>
+        Already have an account?{' '}
+        <button className="login-link" onClick={redirectToLogin}>
+          Login
+        </button>
+      </p>
+    </div>
+  );
 };
+
+export default SignupForm;
